1803906e762cd5fb7004a5e4de15e42768d30538
Dominggoes Isakh <drjisakh@gmail.com>
2020-11-26 22:36:09 +0100

    galaxys2-common: bluetooth: Binderize HIDL

--- common.mk
+++ common.mk
@@ -140,6 +140,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Graphics
