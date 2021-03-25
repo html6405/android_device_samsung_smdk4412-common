54ffff59fd2586db2143f6c6903ed0b7cf34562f
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-03-25 09:11:49 +0100

    Move to legacy gnss hal.
    Change-Id: Ica4f7b6d8432064e6bb56145385e6195a381d136

--- common.mk
+++ common.mk
@@ -109,7 +109,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy \
     android.hardware.sensors@1.0-impl \
-    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-impl.legacy \
     libfimc \
     libfimg \
     libhwconverter \
