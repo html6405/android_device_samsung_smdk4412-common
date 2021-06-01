1114ca8bf4f94ebf4524d3d29f0a19b0c1639cc2
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-06-01 14:01:39 +0200

    HACK: smdk4412-common: add linker loader config and fix build.

--- common.mk
+++ common.mk
@@ -206,6 +206,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mediaserver.rc:system/etc/init/mediaserver.rc
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/ld.config.vndk_lite.txt:/system/etc/ld.config.vndk_lite.txt
+
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/system/etc/media_profiles.xml \
     debug.stagefright.ccodec=0 \
