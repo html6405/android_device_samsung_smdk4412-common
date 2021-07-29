96d9665779d733861dbba5961661acbe50139bd4
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-07-29 12:14:09 +0200

    Included releasetools.py to resize the userdata partition for encryption.

---
+++ releasetools.py
@@ -0,0 +1,10 @@
+import common
+import struct
+
+def FullOTA_PostValidate(info):
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc/by-name/USERDATA");');
+	# resize2fs: run and delete
+	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/dw_mmc/by-name/USERDATA");');
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc/by-name/USERDATA");');
