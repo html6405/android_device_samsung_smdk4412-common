aa9c54bb0fc81eb413079f9bac131c8a67acdb71
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-08-20 13:34:18 +0200

    Resize data to support encryption.

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,6 +1,17 @@
+import common
+import struct
+
 def FullOTA_InstallEnd(info):
 	info.script.AppendExtra('ifelse(is_mounted("/system"), unmount("/system"));')
 	info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/dw_mmc/by-name/SYSTEM", "/system", "");')
 	info.script.AppendExtra('run_program("/sbin/busybox", "rm", "/system/vendor");')
 	info.script.AppendExtra('run_program("/sbin/busybox", "mkdir", "/system/vendor");')
 	info.script.AppendExtra('unmount("/system");')
+
+def FullOTA_PostValidate(info):
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc/by-name/USERDATA");')
+	# resize2fs: run and delete
+	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/dw_mmc/by-name/USERDATA");')
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc/by-name/USERDATA");')
