765de392a87414ae7da737c22780d7607d99fb72
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-09-21 15:16:27 +0200

    param.sh: overwork script and create a backup of the original param bin

--- configs/param.sh
+++ configs/param.sh
@@ -1,7 +1,11 @@
 #!/system/bin/sh
 
 FILE=/data/.param.info
+PARAM_FILE=/data/param.bin
+PARAM_PATH=/dev/block/platform/dw_mmc/by-name/PARAM
+
 file_exists=0
+param_file_exists=0
 
 if test -f "$FILE"; then
   echo "$FILE exists."
@@ -9,6 +13,31 @@ if test -f "$FILE"; then
 fi
 
 if [ "$file_exists" = "0" ] ; then
-  dd if=/vendor/param.lfs of=/dev/block/platform/dw_mmc/by-name/PARAM
-  touch /data/.param.info 0
+ dd if=$PARAM_PATH of=$PARAM_FILE
+
+  if test -f "$PARAM_FILE"; then
+    echo "$FPARAM_FILE exists."
+    param_file_exists=1
+  fi
+
+  if [ "$param_file_exists" = "1" ] ; then
+    dd if=/system/vendor/param.lfs of=$PARAM_PATH
+    touch /data/.param.info 0
+    touch /data/param_restore.sh 0
+    echo "#!/system/bin/sh
+
+    PARAM_FILE=/data/param.bin
+    param_file_exists=0
+    PARAM_PATH=/dev/block/platform/dw_mmc/by-name/PARAM
+
+    if test -f "$PARAM_FILE"; then
+      echo "$FPARAM_FILE exists."
+      param_file_exists=1
+    fi
+
+    if [ "$param_file_exists" = "1" ] ; then
+      dd if=$PARAM_FILE of=$PARAM_PATH
+    fi" > /data/param_restore.sh
+  fi
+
 fi
