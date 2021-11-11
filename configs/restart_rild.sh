5a9b8698fd6fbc6abd4ef4b8672fd14bbac0ed3b
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-11-11 13:48:35 +0100

    restart_rild.sh: clean up.

--- configs/restart_rild.sh
+++ configs/restart_rild.sh
@@ -29,40 +29,34 @@ do_reset_radio() {
 
   if [ "$subId" != "1" ] ; then
     sleep 10
-  fi
-
-  if [ "$subId" != "1" ] ; then
-    stop ril-daemon
-    sleep 5
-    start ril-daemon
-    setprop gsm.resetcount $(( $resetCount + 1 ))
-    resetValue=1
-  fi
-
-  if [ "$simState" != "READY" ] ; then
-    sleep 10
-  fi
-
-  if [ "$resetValue" != "1" ] ; then
-      if [ "$simState" != "READY" ] ; then
-        stop ril-daemon
-        sleep 5
-        start ril-daemon
-        setprop gsm.resetcount $(( $resetCount + 1 ))
-      fi
-    resetValue=0
-  fi
-
-   if [ "$simState" != "READY" ] ; then
-    sleep 10
-    if [ "$simState" != "READY" ] ; then
-        stop ril-daemon
-        sleep 5
-        start ril-daemon
-        setprop gsm.resetcount $(( $resetCount + 1 ))
+    subId=$(getprop gsm.subid)
+    if [ "$subId" != "1" ] ; then
+      stop ril-daemon
+      sleep 5
+      start ril-daemon
+      setprop gsm.resetcount $(( $resetCount + 1 ))
+      resetValue=1
     fi
   fi
 
+  if [ "$resetValue" != "1" ] ; then
+    if [ "$simState" != "READY" ] ; then
+      sleep 10
+      stop ril-daemon
+      sleep 5
+      start ril-daemon
+      setprop gsm.resetcount $(( $resetCount + 1 ))
+    fi
+    sleep 10
+    simState=$(getprop gsm.sim_state)
+    if [ "$simState" != "READY" ] ; then
+      stop ril-daemon
+      sleep 5
+      start ril-daemon
+      setprop gsm.resetcount $(( $resetCount + 1 ))
+    fi
+    resetValue=0
+  fi
   setprop gsm.radioreset false
 
 
