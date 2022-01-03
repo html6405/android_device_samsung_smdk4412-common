11437c116f62132cc68b3c7b31e1ec2ab237e61d
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-01-03 15:25:56 +0100

    smdk4412-common: move OpenCamera to smdk4412-common and disable gCam build.

--- common.mk
+++ common.mk
@@ -170,8 +170,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5-impl \
     android.hardware.camera.provider@2.5-service \
-    camera.device@1.0-impl \
-    gCam
+    camera.device@1.0-impl
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/80cfw:vendor/etc/init.d/80cfw
