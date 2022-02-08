62600fb54030d4cc1b9537158d30d30ceac2b8b9
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-02-08 13:40:26 +0100

    smdk4412-common: introduce fix_perms script for the pegasusq power hal.

--- common.mk
+++ common.mk
@@ -80,6 +80,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/navbar.sh:vendor/bin/navbar.sh \
     $(LOCAL_PATH)/configs/navbar.rc:vendor/etc/init/navbar.rc
 
+# navbar switch
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/navbar.sh:vendor/bin/fix_perms.sh \
+    $(LOCAL_PATH)/configs/navbar.rc:vendor/etc/init/fix_perms.rc
+
 # VNDK
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcutils-v29.so
---
+++ configs/fix_perms.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+
+chmod 0666 /sys/devices/system/cpu/cpufreq/pegasusq/boost_lock_time
