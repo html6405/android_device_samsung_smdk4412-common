9d089d83181612f58348313b1a8af1f6118f4f9c
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-04-12 17:52:38 +0200

    smdk4412-common: releasetools.py: resize system

--- releasetools.py
+++ releasetools.py
@@ -8,3 +8,10 @@ def FullOTA_PostValidate(info):
 	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/dw_mmc/by-name/USERDATA");');
 	# run e2fsck
 	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc/by-name/USERDATA");');
+
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc/by-name/SYSTEM");');
+	# resize2fs: run and delete
+	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/dw_mmc/by-name/SYSTEM");');
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc/by-name/SYSTEM");');
