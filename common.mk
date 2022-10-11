88a37e988e6e303bf4b3b48c4d642f19a6417633
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-10-11 15:24:47 +0200

    Revert "smdk4412-common: use android.hardware.keymaster@3.0-service.samsung."
    This reverts commit 73a9d63d8c7c5111237c97e9a375c8493d1603a4.

--- common.mk
+++ common.mk
@@ -196,7 +196,7 @@ PRODUCT_PACKAGES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service.samsung \
+    android.hardware.keymaster@3.0-service \
     android.hardware.authsecret@1.0-service \
     android.hardware.gatekeeper@1.0-service.software
 
