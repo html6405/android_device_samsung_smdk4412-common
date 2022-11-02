743efe4f59086344a1e95a12f620a38a0f60ff7d
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-11-02 14:43:05 +0100

    smdk4412-common: ro.opengles.version=196608 needed to fix google play store not showing some apps

--- common.mk
+++ common.mk
@@ -385,7 +385,7 @@ PRODUCT_PACKAGES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zygote.disable_gl_preload=1 \
-    ro.opengles.version=131072 \
+    ro.opengles.version=196610 \
     ro.bq.gpu_to_cpu_unsupported=1 \
     debug.hwui.render_dirty_regions=false
 
