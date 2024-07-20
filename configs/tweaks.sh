425aa9666367685540a198d38bc8f60da0cb18cb
html6405 <peter.schelchshorn@mhs-solutions.at>
2024-07-20 21:28:17 +0200

    Revert "smdk4412-common: enable boeffla_sound and set the speaker_volume to max"
    this seems to break the incall receiver volume somehow
    
    This reverts commit d394f80759a0b55396ebf6ea3a4df3c1125b041d.

--- configs/tweaks.sh
+++ configs/tweaks.sh
@@ -2,6 +2,4 @@
 
 echo 1 > /proc/sys/vm/dirty_background_ratio
 echo 300 > /proc/sys/vm/dirty_expire_centisecs
-echo 1 > /sys/class/misc/boeffla_sound/boeffla_sound
-echo "63 63" > /sys/class/misc/boeffla_sound/speaker_volume
 
