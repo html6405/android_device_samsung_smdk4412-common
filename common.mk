162c346ccd5de17b86241168583bb30bb6336754
html6405 <peter.schelchshorn@mhs-solutions.at>
2024-10-19 10:14:46 +0200

    "Revert "smdk4412-common: Enable default PowerHAL v1.0"
    This reverts commit c12602b7b8349a9f59907c7ee5b8fd92de586709.

--- common.mk
+++ common.mk
@@ -95,10 +95,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.small_battery=true
 
-# Power HAL
+# SamsungPowerHAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.0-service.exynos4412
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
