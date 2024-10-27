ac19a621d994b0987a05a3101e7e2395df69f76e
html6405 <peter.schelchshorn@mhs-solutions.at>
2024-10-27 12:11:52 +0100

    smdk4412-common: tweaks.sh: set cpuset configs

--- configs/tweaks.sh
+++ configs/tweaks.sh
@@ -3,3 +3,9 @@
 echo 1 > /proc/sys/vm/dirty_background_ratio
 echo 300 > /proc/sys/vm/dirty_expire_centisecs
 
+#CPUSETS
+echo 0-1 > /dev/cpuset/background/cpus
+echo 0-1 > /dev/cpuset/system-background/cpus
+echo 1 > /dev/cpuset/restricted/cpus
+echo 0-2 > /dev/cpuset/foreground/cpus
+echo 0-3 > /dev/cpuset/top-app/cpus
